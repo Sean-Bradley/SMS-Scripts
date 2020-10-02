50a0e03abb9c34312c5a82e55921dbd153ef1d8d
Sean Bradley <seanwasere@gmail.com>
2020-10-02 07:08:38 +0100

    updates

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # SMS-Scripts
 SMS Scripts
+
+Install dependencies
+
+```bash
+sudo apt install python-pip
+sudo pip install pyserial
+sudo pip install requests
+```
+
+Read SMS
+
+```bash
+sudo python read_sms.py
+```
---
+++ read_sms.py
@@ -0,0 +1,22 @@
+import time
+import serial
+import datetime
+import re
+import requests
+
+phone = serial.Serial("/dev/ttyAMA0", 115200, timeout=1)
+
+try:
+    phone.write(b'ATZ\r')  # set to base user profile
+    print phone.readall()
+
+    phone.write(b"AT+CMGF=1\r")  # set messages to text format
+    print(phone.readall())
+
+    while 1:
+        phone.write(b'AT+CMGL="ALL"\r')
+        data = phone.readall()
+        print(data)
+        time.sleep(5)
+finally:
+    phone.close()
